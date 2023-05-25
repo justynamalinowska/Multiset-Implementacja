56123c4c7b84d144f0140cb0be0a6bb222ad9a53
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-05-25 18:42:03 +0200

    Check a code `correctness`

--- MultiSet/Program.cs
+++ MultiSet/Program.cs
@@ -1,5 +1,4 @@
-﻿using MultiSet;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,43 +13,82 @@ class Program
 {
     static void Main()
     {
-        char[] znaki = new char[] { 'a', 'a', 'b', 'c'};
-        char[] znaki2 = new char[] { 'a', 'b', 'c', 'd', 'e' };
+        char[] znaki = new char[] { 'a', 'a', 'b', 'c', 'd', 'd' };
+        char[] znaki2 = new char[] { 'a', 'b', 'c', 'd' };
         var mz = new MultiSet<char>(znaki);
         var mz2 = new MultiSet<char>(znaki2);
-        Console.WriteLine(mz);
-        Console.WriteLine(mz.ToStringExpanded());
+        var mz3 = new MultiSet<char>();
+        //Console.WriteLine(mz);
+        //Console.WriteLine(mz.ToStringExpanded());
 
-        foreach (var item in mz)
-        {
-            Console.WriteLine(item);
-        }
+        //foreach (var item in mz)
+        //{
+        //    Console.WriteLine(item);
+        //}
 
-        Console.WriteLine(mz.Count());
-        mz.UnionWith(mz2);
-        Console.WriteLine(mz.ToStringExpanded());
+        //Console.WriteLine(mz.Count());
 
-        mz.IntersectWith(mz2);
-        Console.WriteLine(mz.ToStringExpanded());
+        //mz.UnionWith(mz2);
+        //Console.WriteLine(mz.ToStringExpanded());
 
-        mz.Add('d', 5);
-        Console.WriteLine("+5*d:");
-        Console.WriteLine(mz.ToStringExpanded());
+        //mz.IntersectWith(mz2);
+        //Console.WriteLine(mz.ToStringExpanded());
 
-        mz.Remove('d', 2);
-        Console.WriteLine(mz.ToStringExpanded());
+        //mz.Add('d', 5);
+        //Console.WriteLine("+5*d:");
+        //Console.WriteLine(mz.ToStringExpanded());
 
-        mz.RemoveAll('d');
-        Console.WriteLine(mz.ToStringExpanded());
+        //mz.Remove('d', 2);
+        //Console.WriteLine(mz.ToStringExpanded());
 
-        char[] chars = new char[znaki.Length];
+        //mz.RemoveAll('d');
+        //Console.WriteLine(mz.ToStringExpanded());
 
-        znaki.CopyTo(chars, 0);
-        var charss = new MultiSet<char>(chars);
-        Console.WriteLine(charss.ToStringExpanded());
+        //char[] chars = new char[znaki.Length];
 
+        //znaki.CopyTo(chars, 0);
+        //var charss = new MultiSet<char>(chars);
+        //Console.WriteLine(charss.ToStringExpanded());
 
-        mz.Clear();
+        //mz.ExceptWith(mz2);
+        //Console.WriteLine(mz.ToStringExpanded());
+
+        //mz.SymmetricExceptWith(mz2);
+        //Console.WriteLine(mz.ToStringExpanded());
+
+        //Console.WriteLine(mz2.IsSubsetOf(mz));
+
+        //Console.WriteLine(mz2.IsProperSubsetOf(mz));
+
+        //Console.WriteLine(mz2.IsSupersetOf(mz));
+
+        //Console.WriteLine(mz.IsProperSupersetOf(mz2));
+
+        //Console.WriteLine(mz2.Overlaps(mz));
+
+        //Console.WriteLine(mz2.MultiSetEquals(mz));
+
+        //Console.WriteLine(mz2.IsEmpty);
+
+        //Console.WriteLine(mz['a']);
+
+        //mz.AsDictionary().ToString();
+        //Console.WriteLine(mz['a']);
+
+        //mz.AsSet();
+        //Console.WriteLine(mz.ToStringExpanded());
+
+        //Console.WriteLine(MultiSet<char>.Empty.ToString());
+
+        //Console.WriteLine(mz3.ToString());
+
+        //Console.WriteLine((mz + mz2).ToStringExpanded());
+
+        //Console.WriteLine((mz - mz2).ToStringExpanded());
+
+        //Console.WriteLine((mz * mz2).ToStringExpanded());
+
+        //mz.Clear();
         
     }
 }
