0f9a356add7806cac8f4f345b21be4901b396d4e
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-05-25 19:30:49 +0200

    Add tests for `Count()` and `IsEmpty`

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -40,12 +40,12 @@ namespace Tests
         [DataRow('i', 'j', 'k', 'l')]
         public void Char(char ch1, char ch2, char ch3, char ch4)
         {
-            var list = new List<char> { ch1, ch2, ch3 };
+            var list = new List<char> { ch1, ch2, ch3, ch4 };
             var actual = new MultiSet<char>(list);
             Assert.AreEqual(actual.Contains(ch1), true);
             Assert.AreEqual(actual.Contains(ch2), true);
             Assert.AreEqual(actual.Contains(ch3), true);
-            Assert.AreEqual(actual.Contains(ch4), false);
+            Assert.AreEqual(actual.Contains(ch4), true);
         }
 
         [DataTestMethod]
@@ -60,6 +60,45 @@ namespace Tests
             Assert.AreEqual(actual.Contains(s2), true);
             Assert.AreEqual(actual.Contains(s3), true);
             Assert.AreEqual(actual.Contains(s4), true);
+        }
+    }
+    
+    [TestClass]
+    public class MethodsFromICollection
+    {
+
+        [DataTestMethod]
+        [DataRow('a', 'b', 'c', 'd', 4)]
+        public void CharCount(char ch1, char ch2, char ch3, char ch4, int length)
+        {
+            var list = new List<char> { ch1, ch2, ch3, ch4 };
+            var set = new MultiSet<char>(list);
+            Assert.AreEqual(set.Count(), length);
+        }
+
+        [DataTestMethod]
+        [DataRow("aaa", "bbb", "ccc", "ddd", 4)]
+        public void StringCount(string s1, string s2, string s3, string s4, int length)
+        {
+            var list = new List<string> { s1, s2, s3, s4 };
+            var set = new MultiSet<string>(list);
+            Assert.AreEqual(set.Count(), length);
+        }
+
+        [DataTestMethod]
+        public void IsEmpty()
+        {
+            var set = new MultiSet<string>();
+            Assert.AreEqual(set.IsEmpty, true);
+        }
+
+        [DataTestMethod]
+        [DataRow("aaa", "bbb", "ccc", "ddd")]
+        public void IsNotEmpty(string s1, string s2, string s3, string s4)
+        {
+            var list = new List<string> { s1, s2, s3, s4 };
+            var set = new MultiSet<string>(list);
+            Assert.AreEqual(set.IsEmpty, false);
         }
     }
 }
